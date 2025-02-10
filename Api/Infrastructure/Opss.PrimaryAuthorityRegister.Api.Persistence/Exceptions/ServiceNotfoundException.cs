51cd52194f861af3327b257027ed64b444f03a1c
Richard Priddy <richard.priddy@triad.co.uk>
2025-02-10 17:12:58 +0000

    Dereference of a possibly null reference.
    If the service cannot be found, throw an exception

---
+++ Api/Infrastructure/Opss.PrimaryAuthorityRegister.Api.Persistence/Exceptions/ServiceNotfoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace Opss.PrimaryAuthorityRegister.Api.Persistence.Exceptions;
+
+public class ServiceNotfoundException : Exception
+{
+    public ServiceNotfoundException(string message)
+        : base($"Service: {message} not found")
+    {
+    }
+
+    public ServiceNotfoundException(string message, Exception innerException)
+        : base($"Service: {message} not found", innerException)
+    {
+    }
+}
