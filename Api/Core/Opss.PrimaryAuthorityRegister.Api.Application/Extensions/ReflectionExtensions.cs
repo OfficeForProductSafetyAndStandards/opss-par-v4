de6f3380479b53db1b601a5e9c8a8d325c23ea54
Richard Priddy <richard.priddy@triad.co.uk>
2025-02-10 16:49:01 +0000

    Length != 0 preferred over Any

--- Api/Core/Opss.PrimaryAuthorityRegister.Api.Application/Extensions/ReflectionExtensions.cs
+++ Api/Core/Opss.PrimaryAuthorityRegister.Api.Application/Extensions/ReflectionExtensions.cs
@@ -15,6 +15,8 @@ public static class ReflectionExtensions
     /// <returns>Whether or not the specified type is a Nullable type.</returns>
     public static bool CanBeNull(this Type type)
     {
+        ArgumentNullException.ThrowIfNull(type);
+        
         return !type.IsPrimitive;
     }
 
@@ -29,6 +31,7 @@ public static class ReflectionExtensions
     public static IEnumerable<T> GetAttributes<T>(this MemberInfo memberInfo, bool inherit)
     {
         ArgumentNullException.ThrowIfNull(memberInfo);
+
         return memberInfo.GetCustomAttributes(typeof(T), inherit).Cast<T>();
     }
 
@@ -60,7 +63,9 @@ public static class ReflectionExtensions
     /// <returns>Whether this member has any attributes of the specified type.</returns>
     public static bool HasAttribute<T>(this MemberInfo memberInfo, bool inherit = true)
     {
-        return memberInfo.GetCustomAttributes(typeof(T), inherit).Any();
+        ArgumentNullException.ThrowIfNull(memberInfo);
+        
+        return memberInfo.GetCustomAttributes(typeof(T), inherit).Length != 0;
     }
 
     /// <summary>
@@ -73,7 +78,9 @@ public static class ReflectionExtensions
     /// <returns>Whether this member has any attributes of the specified type.</returns>
     public static bool HasAttribute(this MemberInfo memberInfo, Type attributeType, bool inherit = true)
     {
-        return memberInfo.GetCustomAttributes(attributeType, inherit).Any();
+        ArgumentNullException.ThrowIfNull(memberInfo);
+        
+        return memberInfo.GetCustomAttributes(attributeType, inherit).Length != 0;
     }
 
     /// <summary>
@@ -85,6 +92,8 @@ public static class ReflectionExtensions
     /// <returns>Whether or not the specified type is an IEnumerable type.</returns>
     public static bool IsEnumerable(this Type type)
     {
+        ArgumentNullException.ThrowIfNull(type);
+        
         return type != typeof(string) && type.GetInterface("IEnumerable") != null;
     }
 }
