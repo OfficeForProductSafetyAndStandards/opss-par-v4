3b3a5272ebceb21612f919ff28a2c8ddb0057376
Richard Priddy <richard.priddy@triad.co.uk>
2025-02-27 15:09:59 +0000

    Corrected roles

--- Common/Opss.PrimaryAuthorityRegister.Common/Constants/IdentityConstants.cs
+++ Common/Opss.PrimaryAuthorityRegister.Common/Constants/IdentityConstants.cs
@@ -4,9 +4,28 @@ public static class IdentityConstants
 {
     public static class Roles
     {
-        public const string Authority = "Authority";
-        public const string LAU = "LAU";
+        public const string AuthorityManager = "Primary Authority Officer - Manager";
+        public const string AuthorityMember = "Primary Authority Officer - Member";
+        public const string EnforcingManager = "Enforcing Officer - Manager";
+        public const string EnforcingMember = "Enforcing Officer - Member";
+        public const string OrganisationManager = "Organisation - Manager";
+        public const string OrganisationMember = "Organisation - Member";
+        public const string Helpdesk = "Helpdesk Officer";
+        public const string SeniorHelpdesk = "Senior Helpdesk Officer";
+        public const string SecretaryOfState = "Secretary of State";
+        public const string LAU = "Local authority unit (LAU) Officer";
 
-        public readonly static string[] AllRoles = { Authority, LAU };
+        public readonly static string[] AllRoles = {
+            AuthorityManager,
+            AuthorityMember,
+            EnforcingManager,
+            EnforcingMember,
+            OrganisationManager,
+            OrganisationMember,
+            Helpdesk,
+            SeniorHelpdesk,
+            SecretaryOfState,
+            LAU
+        };
     }
 }
