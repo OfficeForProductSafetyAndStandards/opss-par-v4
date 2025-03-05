019feefcc4b23aa5441ac0316db024f310262aa6
Richard Priddy <richard.priddy@triad.co.uk>
2025-03-05 15:42:31 +0000

    #567 & #568
    This PR covers both tickets.
    I've also abstracted the QueryHelpers into a singleton allowing for DI and testing of a page by mocking the IQueryHelper.

---
+++ Web/Core/Opss.PrimaryAuthorityRegister.Web.Application/Helpers/IQueryHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace Opss.PrimaryAuthorityRegister.Web.Application.Helpers;
+
+public interface IQueryHelper
+{
+    /// <summary>
+    /// Parse a query string into its component key and value parts.
+    /// </summary>
+    /// <param name="queryString">The raw query string value, with or without the leading '?'.</param>
+    /// <returns>A collection of parsed keys and values.</returns>
+    Dictionary<string, StringValues> ParseQuery(string? queryString);
+
+    /// <summary>
+    /// Parse a query string into its component key and value parts.
+    /// </summary>
+    /// <param name="queryString">The raw query string value, with or without the leading '?'.</param>
+    /// <returns>A collection of parsed keys and values, null if there are no entries.</returns>
+    Dictionary<string, StringValues>? ParseNullableQuery(string? queryString);
+}
