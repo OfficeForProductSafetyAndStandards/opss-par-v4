5999a287ad149c48e2783016fec583a472885d6f
Scotty Wakefield <scottywakefield@users.noreply.github.com>
2025-03-07 18:03:17 +0000

    adding db migration

---
+++ Api/Infrastructure/Opss.PrimaryAuthorityRegister.Api.Persistence/Migrations/20250307180021_UserProfile_Table.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Opss.PrimaryAuthorityRegister.Api.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserProfile_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserProfile",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserIdentityId = table.Column<Guid>(type: "uuid", nullable: true),
+                    HasAcceptedTermsAndConditions = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamptz", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamptz", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfile", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserProfile_UserIdentities_UserIdentityId",
+                        column: x => x.UserIdentityId,
+                        principalTable: "UserIdentities",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfile_UserIdentityId",
+                table: "UserProfile",
+                column: "UserIdentityId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserProfile");
+        }
+    }
+}
