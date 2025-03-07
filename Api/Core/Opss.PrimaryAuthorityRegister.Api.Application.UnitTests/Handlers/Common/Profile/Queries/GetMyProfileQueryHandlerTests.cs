80a6811882a152fe91d7c6a86e86dcda63e202ab
Scotty Wakefield <scottywakefield@users.noreply.github.com>
2025-03-07 11:41:33 +0000

    handler and tests done for now

--- Api/Core/Opss.PrimaryAuthorityRegister.Api.Application.UnitTests/Handlers/Common/Profile/Queries/GetMyProfileQueryHandlerTests.cs
+++ Api/Core/Opss.PrimaryAuthorityRegister.Api.Application.UnitTests/Handlers/Common/Profile/Queries/GetMyProfileQueryHandlerTests.cs
@@ -3,26 +3,27 @@ using Opss.PrimaryAuthorityRegister.Api.Application.Handlers.Common.Profile.Quer
 using Opss.PrimaryAuthorityRegister.Api.Application.Interfaces.Repositories;
 using Opss.PrimaryAuthorityRegister.Cqrs.Requests.Common.Profile.Queries;
 using Opss.PrimaryAuthorityRegister.Http.Exceptions;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace Opss.PrimaryAuthorityRegister.Api.Application.UnitTests.Handlers.Common.Profile.Queries
 {
     public class GetMyProfileQueryHandlerTests
     {
-        private readonly Mock<IGenericRepository<Domain.Entities.Authority>> _repo;
+        private readonly Mock<IGenericRepository<Domain.Entities.UserProfile>> _mockProfileRepository;
         private ClaimsPrincipal? _claimsPrincipal;
 
         public GetMyProfileQueryHandlerTests()
         {
-            _repo = new Mock<IGenericRepository<Domain.Entities.Authority>>();
+            _mockProfileRepository = new Mock<IGenericRepository<Domain.Entities.UserProfile>>();
         }
 
         [Fact]
-        public async Task WhenNullClaimsPrincipal_ThenUnauthorizedExceptionThrown()
+        public async Task WhenNullClaimsPrincipal_ThenHttpUnauthorizedExceptionThrown()
         {
             // Arrange
             _claimsPrincipal = null;
-            var handler = new GetMyProfileQueryHandler(_repo.Object, _claimsPrincipal);
+            var handler = new GetMyProfileQueryHandler(_mockProfileRepository.Object, _claimsPrincipal);
 
             // Act & Assert
             var exception = await Assert.ThrowsAsync<HttpResponseException>(() =>
@@ -30,14 +31,15 @@ namespace Opss.PrimaryAuthorityRegister.Api.Application.UnitTests.Handlers.Commo
                                     CancellationToken.None));
 
             Assert.Equal("You are not authenticated", exception.Message);
+            Assert.Equal(System.Net.HttpStatusCode.Unauthorized, exception.Response.StatusCode);
         }
         
         [Fact]
-        public async Task WhenNoUserId_ThenUnauthorizedExceptionThrown()
+        public async Task WhenNoUserId_ThenHttpBadRequestExceptionThrown()
         {
             // Arrange
             _claimsPrincipal = new ClaimsPrincipal();
-            var handler = new GetMyProfileQueryHandler(_repo.Object, _claimsPrincipal);
+            var handler = new GetMyProfileQueryHandler(_mockProfileRepository.Object, _claimsPrincipal);
 
             // Act & Assert
             var exception = await Assert.ThrowsAsync<HttpResponseException>(() =>
@@ -45,6 +47,60 @@ namespace Opss.PrimaryAuthorityRegister.Api.Application.UnitTests.Handlers.Commo
                     CancellationToken.None));
 
             Assert.Equal("Your userId was not found", exception.Message);
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, exception.Response.StatusCode);
+        }
+        
+        [Fact]
+        public async Task WhenNoProfile_ThenHttpNotFoundExceptionThrown()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            _claimsPrincipal = new ClaimsPrincipal();
+            _claimsPrincipal.AddIdentity(new ClaimsIdentity(new List<Claim>
+            {
+                new (JwtRegisteredClaimNames.Sid, userId.ToString())
+            }));
+            
+            _mockProfileRepository
+                .Setup(r => r.GetByIdAsync(It.Is<Guid>(guid => guid == userId)))
+                .ReturnsAsync(() => null);
+            
+            var handler = new GetMyProfileQueryHandler(_mockProfileRepository.Object, _claimsPrincipal);
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<HttpResponseException>(() =>
+                handler.Handle(new GetMyProfileQuery(),
+                    CancellationToken.None));
+
+            Assert.Equal("Your profile was not found", exception.Message);
+            Assert.Equal(System.Net.HttpStatusCode.NotFound, exception.Response.StatusCode);
+        }
+        
+        [Fact]
+        public async Task GivenQuery_ThenDataReturned()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            _claimsPrincipal = new ClaimsPrincipal();
+            _claimsPrincipal.AddIdentity(new ClaimsIdentity(new List<Claim>
+            {
+                new (JwtRegisteredClaimNames.Sid, userId.ToString())
+            }));
+
+            var profile = new Domain.Entities.UserProfile{HasAcceptedTermsAndConditions = true, Id = userId};
+
+            _mockProfileRepository
+                .Setup(r => r.GetByIdAsync(It.Is<Guid>(guid => guid == userId)))
+                .ReturnsAsync(() => profile);
+
+            var handler = new GetMyProfileQueryHandler(_mockProfileRepository.Object, _claimsPrincipal);
+
+            // Act
+            var result = await handler.Handle(new GetMyProfileQuery(), CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.HasAcceptedTermsAndConditions);
         }
     }
 }
--- Api/Core/Opss.PrimaryAuthorityRegister.Api.Application/Handlers/Common/Profile/Queries/GetMyProfileQueryHandler.cs
+++ Api/Core/Opss.PrimaryAuthorityRegister.Api.Application/Handlers/Common/Profile/Queries/GetMyProfileQueryHandler.cs
@@ -10,25 +10,31 @@ namespace Opss.PrimaryAuthorityRegister.Api.Application.Handlers.Common.Profile.
     public class GetMyProfileQueryHandler
         : IRequestHandler<GetMyProfileQuery, MyProfileDto>
     {
-        private readonly IGenericRepository<Domain.Entities.Authority> _genericRepository;
+        private readonly IGenericRepository<Domain.Entities.UserProfile> _profileRepository;
         private readonly ClaimsPrincipal? _claimsPrincipal;
 
-        public GetMyProfileQueryHandler(IGenericRepository<Domain.Entities.Authority> genericRepository, ClaimsPrincipal? claimsPrincipal)
+        public GetMyProfileQueryHandler(IGenericRepository<Domain.Entities.UserProfile> profileRepository, ClaimsPrincipal? claimsPrincipal)
         {
-            _genericRepository = genericRepository;
+            _profileRepository = profileRepository;
             _claimsPrincipal = claimsPrincipal;
         }
 
-        public Task<MyProfileDto> Handle(GetMyProfileQuery request, CancellationToken cancellationToken)
+        public async Task<MyProfileDto> Handle(GetMyProfileQuery request, CancellationToken cancellationToken)
         {
             if (_claimsPrincipal == null)
                 throw new HttpResponseException(System.Net.HttpStatusCode.Unauthorized, "You are not authenticated");
 
             var userId = _claimsPrincipal.GetUserId();
             if (userId == null)
-                throw new HttpResponseException(System.Net.HttpStatusCode.Unauthorized, "Your userId was not found");
+                throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest, "Your userId was not found");
 
-            return new Task<MyProfileDto>(null);
+            var profile = await _profileRepository.GetByIdAsync(userId.Value).ConfigureAwait(false);
+            if (profile == null)
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound, "Your profile was not found");
+
+            var profileDto = new MyProfileDto(profile.HasAcceptedTermsAndConditions);
+
+            return profileDto;
         }
     }
 }
